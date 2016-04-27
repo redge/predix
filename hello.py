0b3ef55f46a940df116df9040b0a3b2bd313b3f9
luisfisherjr <luisfisherjr@outlook.com>
2016-04-26 21:07:04 -0700

    break up display over multiple pages

--- hello.py
+++ hello.py
@@ -17,16 +17,95 @@ app = Flask(__name__)
 
 port = int(os.getenv("PORT", 64781))
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/tsConsumed', methods=["GET", "POST"])
 @crossdomain(origin='*')
-def hello_world():
+def hello_world1():
     #X = np.array([[ 1., -1.,  2.],[ 2.,  0.,  0.],[ 0.,  1., -1.]])
     #X_scaled = preprocessing.scale(X)
     
-    tmp = str(red.getVar('tsConsumed')) + str(red.getVar('tsCostWD')) + str(red.getVar('tsCostWOD')) + str(red.getVar('tsSolarP')) + str(red.getVar('tsSolarLux')) + str(red.getVar('tsTemperature')) + str(red.getVar('tsPressure')) + str(red.getVar('tsHumidity')) + str(red.getVar('tsWindSpeed'))
-    
+    tmp = str(red.getVar('tsConsumed'))
     resp = Response(response=tmp, status=200, mimetype="application/json")
     return resp
+
+@app.route('/tsCostWD', methods=["GET", "POST"])
+@crossdomain(origin='*')
+def hello_world2():
+    #X = np.array([[ 1., -1.,  2.],[ 2.,  0.,  0.],[ 0.,  1., -1.]])
+    #X_scaled = preprocessing.scale(X)
+    
+    tmp = str(red.getVar('tsCostWD'))
+    resp = Response(response=tmp, status=200, mimetype="application/json")
+    return resp
+    
+@app.route('/tsCostWOD', methods=["GET", "POST"])
+@crossdomain(origin='*')    
+def hello_world3():
+    #X = np.array([[ 1., -1.,  2.],[ 2.,  0.,  0.],[ 0.,  1., -1.]])
+    #X_scaled = preprocessing.scale(X)
+    
+    tmp = str(red.getVar('tsCostWOD'))
+    resp = Response(response=tmp, status=200, mimetype="application/json")
+    return resp
+    
+@app.route('/tsSolarP', methods=["GET", "POST"])
+@crossdomain(origin='*')    
+def hello_world4():
+    #X = np.array([[ 1., -1.,  2.],[ 2.,  0.,  0.],[ 0.,  1., -1.]])
+    #X_scaled = preprocessing.scale(X)
+    
+    tmp = str(red.getVar('tsSolarP'))
+    resp = Response(response=tmp, status=200, mimetype="application/json")
+    return resp
+    
+@app.route('/tsWindSpeed', methods=["GET", "POST"])
+@crossdomain(origin='*')
+def hello_world5():
+    #X = np.array([[ 1., -1.,  2.],[ 2.,  0.,  0.],[ 0.,  1., -1.]])
+    #X_scaled = preprocessing.scale(X)
+    
+    tmp = str(red.getVar('tsWindSpeed'))
+    resp = Response(response=tmp, status=200, mimetype="application/json")
+    return resp
+
+@app.route('/tsHumidity', methods=["GET", "POST"])
+@crossdomain(origin='*')
+def hello_world6():
+    #X = np.array([[ 1., -1.,  2.],[ 2.,  0.,  0.],[ 0.,  1., -1.]])
+    #X_scaled = preprocessing.scale(X)
+    
+    tmp = str(red.getVar('tsHumidity'))
+    resp = Response(response=tmp, status=200, mimetype="application/json")
+    return resp
+
+@app.route('/tsPressure', methods=["GET", "POST"])
+@crossdomain(origin='*')
+def hello_world7():
+    #X = np.array([[ 1., -1.,  2.],[ 2.,  0.,  0.],[ 0.,  1., -1.]])
+    #X_scaled = preprocessing.scale(X)
+    
+    tmp = str(red.getVar('tsPressure'))
+    resp = Response(response=tmp, status=200, mimetype="application/json")
+    return resp
+
+@app.route('/tsTemperature', methods=["GET", "POST"])
+@crossdomain(origin='*')
+def hello_world8():
+    #X = np.array([[ 1., -1.,  2.],[ 2.,  0.,  0.],[ 0.,  1., -1.]])
+    #X_scaled = preprocessing.scale(X)
+    
+    tmp = str(red.getVar('tsTemperature'))
+    resp = Response(response=tmp, status=200, mimetype="application/json")
+    return resp
+
+@app.route('/tsSolarLux', methods=["GET", "POST"])
+@crossdomain(origin='*')
+def hello_world9():
+    #X = np.array([[ 1., -1.,  2.],[ 2.,  0.,  0.],[ 0.,  1., -1.]])
+    #X_scaled = preprocessing.scale(X)
+    
+    tmp = str(red.getVar('tsSolarLux'))
+    resp = Response(response=tmp, status=200, mimetype="application/json")
+    return resp    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
